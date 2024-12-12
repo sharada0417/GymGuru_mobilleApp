1a884c9ddd0a228a239a8f0c90b614af97fa165e
sharada0417 <sharada.kanchana417@gmail.com>
2024-12-13 02:36:17 +0530

    create colors;

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kSubtitleColor = Color.fromARGB(255, 118, 115, 115);
+const KMainWhiteColor = Colors.white;
+const KMainColor = Color.fromARGB(255, 7, 243, 58);
+
+const kMainBlackColor = Colors.black12;
+const KMainDarkGreenColor = Color.fromARGB(255, 4, 144, 34);
+const KMainRedColor = Color.fromARGB(255, 234, 34, 34);
+
+const KMainLightYellowColor = Color.fromARGB(235, 226, 241, 7);
+const KSubRedColor = Color.fromARGB(255, 222, 50, 50);
+const KSubBluColor = Color.fromARGB(255, 3, 126, 227);
